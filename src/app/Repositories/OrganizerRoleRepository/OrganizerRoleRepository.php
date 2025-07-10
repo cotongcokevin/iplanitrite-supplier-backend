dfd99e49972d6a5df69b99ed3d1e688840d6ce48
Kevin Cotongco <cotongcokevin@gmail.com>
2025-07-11 00:40:27 +0930

    organizer role

---
+++ src/app/Repositories/OrganizerRoleRepository/OrganizerRoleRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repositories\OrganizerRoleRepository;
+
+use App\Classes\Accountable;
+use Ramsey\Uuid\UuidFactory;
+
+class OrganizerRoleRepository
+{
+    private Accountable $accountable;
+
+    private UuidFactory $uuid;
+
+    public function __construct(
+        Accountable $accountable,
+        UuidFactory $uuid
+    ) {
+        $this->accountable = $accountable;
+        $this->uuid = $uuid;
+    }
+}

5d937dde9a6d517a3708754b05c7adfdc95cd73f
Drew <llorera.andrew@gmail.com>
2025-07-12 23:12:49 +0930

    use foreignUuid

--- src/database/migrations/0000002_create_contact_number_table.php
+++ src/database/migrations/0000002_create_contact_number_table.php
@@ -14,8 +14,7 @@ return new class extends Migration
         Schema::create('contact_number', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->string('phone_number', 20)->unique();
-            $table->uuid('country_id');
-            $table->foreign('country_id')->references('id')->on('country');
+            $table->foreignUuid('country_id')->constrained('country');
         });
     }
 
